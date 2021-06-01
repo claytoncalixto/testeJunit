72960cf26e2328ac11d3d52842ae6404d0f6a9e7
claytoncalixto <claytonrcalixto@gmail.com>
2021-06-01 18:33:14 -0300

    depositShouldIncreaseBalancePositiveAmount

--- teste-junit/src/entities/Account.java
+++ teste-junit/src/entities/Account.java
@@ -1,12 +1,12 @@
 package entities;
 
 public class Account {
-	
+
 	public static double DEPOSIT_FEE_PERCENTAGE = 0.02;
-	
+
 	private Long id;
 	private Double balance;
-	
+
 	public Account() {
 	}
 
@@ -26,8 +26,24 @@ public class Account {
 	public Double getBalance() {
 		return balance;
 	}
-	
+
 	public void deposit(double amount) {
-		
+		if (amount > 0) {
+			amount -= amount * DEPOSIT_FEE_PERCENTAGE;
+			balance += amount;
+		}
+	}
+
+	public void withdraw(double amount) {
+		if (amount > balance) {
+			throw new IllegalArgumentException();
+		}
+		balance -= amount;
+	}
+
+	public double fullWithdraw() {
+		double aux = balance;
+		balance = 0.0;
+		return aux;
 	}
 }
